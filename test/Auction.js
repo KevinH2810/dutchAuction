907ef1f31d3b0417771a85fd8996df74de002e8e
Kevin Hadi <47102992+KevinH2810@users.noreply.github.com>
2021-11-19 15:29:28 +0700

    add more test case

--- test/Auction.js
+++ test/Auction.js
@@ -39,8 +39,6 @@ describe("Auction", (accounts) => {
 			6,
 			token.address
 		);
-		let x = await auction.owner();
-		let amount = await auction.auctionedAmount();
 
 		token = Token.attach(token.address);
 		//set the allowance
@@ -49,20 +47,100 @@ describe("Auction", (accounts) => {
 		});
 
 		//check the allowance
-		const contractAllowance = await token.allowance(
-			accounts[0].address,
-			auction.address
-		);
+		await token.allowance(accounts[0].address, auction.address);
 		//bid the item
-		const res = await auction.bid(
-			accounts[1].address,
-			ethers.utils.parseUnits("50", "wei")
-		);
+		auction = Auction.attach(auction.address);
+		const res = await auction
+			.connect(accounts[0])
+			.bid(accounts[1].address, ethers.utils.parseUnits("50", "wei"));
 
 		const balance1 = await token.balanceOf(accounts[1].address);
 		const balance0 = await token.balanceOf(accounts[0].address);
 
 		expect(balance1.toNumber()).to.equal(6);
 		expect(balance0.toNumber()).to.equal(94);
+	});
+
+	it("Auction - test Bid function - Fail - Bid price is lower than current price", async () => {
+		const accounts = await hre.ethers.getSigners();
+		const Token = await ethers.getContractFactory("Token");
+		const Auction = await ethers.getContractFactory("Auction");
+
+		token = await Token.deploy();
+		auction = await Auction.deploy(
+			45,
+			Math.floor(Date.now() / 1000 - 3600),
+			Math.floor(Date.now() / 1000 + 259200),
+			6,
+			token.address
+		);
+
+		token = Token.attach(token.address);
+		//set the allowance
+		await token.approve(auction.address, ethers.utils.parseUnits("10", "wei"), {
+			from: accounts[0].address,
+		});
+
+		//check the allowance
+		await token.allowance(accounts[0].address, auction.address);
+		//bid the item
+		await expect(
+			auction
+				.connect(accounts[0])
+				.bid(accounts[1].address, ethers.utils.parseUnits("1", "wei"))
+		).to.be.revertedWith("amount is lower than price");
+	});
+
+	it("Auction - test Bid function - Fail - Auction is not started yet", async () => {
+		const accounts = await hre.ethers.getSigners();
+		const Token = await ethers.getContractFactory("Token");
+		const Auction = await ethers.getContractFactory("Auction");
+
+		token = await Token.deploy();
+		auction = await Auction.deploy(
+			45,
+			Math.floor(Date.now() / 1000 + 14400),
+			Math.floor(Date.now() / 1000 + 259200),
+			6,
+			token.address
+		);
+		await expect(
+			auction
+				.connect(accounts[0])
+				.bid(accounts[1].address, ethers.utils.parseUnits("55", "wei"))
+		).to.be.revertedWith("Auction hasn't started yet");
+	});
+
+	it("Auction - test Bid function - Fail - Owner participate in bidding", async () => {
+		const accounts = await hre.ethers.getSigners();
+		const Token = await ethers.getContractFactory("Token");
+		const Auction = await ethers.getContractFactory("Auction");
+
+		token = await Token.deploy();
+
+		auction = await Auction.deploy(
+			45,
+			Math.floor(Date.now() / 1000 - 3600),
+			Math.floor(Date.now() / 1000 + 259200),
+			6,
+			token.address
+		);
+
+		token = Token.attach(token.address);
+		//set the allowance
+		await token.approve(auction.address, ethers.utils.parseUnits("10", "wei"), {
+			from: accounts[0].address,
+		});
+
+		//check the allowance
+		await token.allowance(accounts[0].address, auction.address);
+		//bid the item
+		expect(
+			auction
+				.connect(accounts[0])
+				.bid(accounts[0].address, ethers.utils.parseUnits("60", "wei"))
+		).to.be.revertedWith(
+			"'owner are now allowed to participate in the Auction"
+		);
 	});
 });
